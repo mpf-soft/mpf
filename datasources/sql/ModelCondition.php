e9a9476e4e9166926c69a8e92e089338fbc856f8
Mirel Mitache <mirel.mitache@avira.com>
2015-05-06 14:40:44 +0300

    Fixed manual join  + with in a ModelCondition

--- datasources/sql/ModelCondition.php
+++ datasources/sql/ModelCondition.php
@@ -431,7 +431,7 @@ class ModelCondition extends \mpf\base\LogAwareObject {
         if (!$this->relationsParser) {
             $this->relationsParser = RelationsParser::parse($this->model, $this, $this->conditionColumns);
         }
-        return $this->join . ($forCount?$this->relationsParser->getForCount():$this->relationsParser->getForMainSelect());
+        return $this->join . ' ' . ($forCount?$this->relationsParser->getForCount():$this->relationsParser->getForMainSelect());
     }
 
     public function getJoinParams(){
