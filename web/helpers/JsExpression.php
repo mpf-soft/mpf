7677b2efabb1f0334b73019e816b067fd2de4ca8
Mirel Mitache <mirel.mitache@avira.com>
2016-02-01 14:01:57 +0200

    Add a JSON class and a helper class for it.

---
+++ web/helpers/JsExpression.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mirel
+ * Date: 01.02.2016
+ * Time: 13:37
+ */
+
+namespace mpf\web\helpers;
+
+
+use mpf\base\Object;
+
+class JsExpression extends Object {
+    /**
+     * @var string
+     */
+    public $expression;
+
+    public static function get($expression) {
+        return new self(['expression' => $expression]);
+    }
+
+    public function __toString() {
+        return $this->expression;
+    }
+}
