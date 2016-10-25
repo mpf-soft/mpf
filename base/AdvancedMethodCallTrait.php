951eec0e74504c31479c9d48191517126d1783e9
Mirel Mitache <mirel.mitache@gmail.com>
2016-10-25 20:54:30 +0300

    Add description for AdvancedMethodCallTrait

--- base/AdvancedMethodCallTrait.php
+++ base/AdvancedMethodCallTrait.php
@@ -28,8 +28,46 @@
 
 namespace mpf\base;
 
+/**
+ * This trait offers 2 methods that help to call a callable function using an associated array as parameter so that
+ * the order won't matter.
+ *
+ * This is used mainly by {@class:\mpf\web\Controller} and {@class:\mpf\cli\Command} to easily call actions using the
+ * parameters sent to the app when executed(`$_GET` for web applications and console arguments for terminal applications).
+ *
+ * Example:
+ * [php]class Test{
+ *   use AdvancedMethodCallTrait;
+ *
+ *   function toTest($a=0, $b = 'something', $c = 'nothing'){
+ *     return "[a={$a}, b={$b} and c = {$c}]";
+ *   }
+ * }
+ *
+ *
+ *
+ * $test = new Test();
+ * echo $test->callMethod('toTest', ['c' => 'everything']);
+ * // will output: "[a=0, b=something and c = everything]"
+ * [/php]
+ *
+ * It does require 2 other methods to be defined in the class that uses this trait:
+ *
+ *   - `critical()`  to log any errors that could occur. Any {@class:\mpf\base\LogAwareObject} child should be fine.
+ *   - `applyParamsToClass()` - this can be used to apply some of the parameters to the class attributes also. Must be careful to
+ * not not overwrite any important attributes. This is used by {@class:\mpf\cli\Command} to apply some of the arguments sent in terminal to
+ * current command and allows some general options like `--debug` to be available for all commands and actions. It is not recommended
+ * for websites where there is no control over what the user input will be, for that a dummy method should be created that won't apply any changes.
+ *
+ * For details about the default uses of this trait check : {@method:\mpf\cli\Command::run()} and {@methood:\mpf\web\Controller::run()} descriptions.
+ */
 trait AdvancedMethodCallTrait {
 
+    /**
+     * Records if there was an error or not when trying to call the method.
+     *
+     * @var bool
+     */
     private $error;
     
     /**
@@ -75,9 +113,9 @@ trait AdvancedMethodCallTrait {
      * @return mixed
      */
     public function callMethod($methodName, $options) {
-        $error = false;
+        $this->error = false;
         if (null === ($options = $this->getMethodParameters($methodName, $options))) {
-            $error = true;
+            $this->error = true;
             return null;
         }
 
