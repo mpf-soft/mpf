2f8e933f67193ee92c854bf6a73e42117b370d5f
Mirel Mitache <mirel.mitache@avira.com>
2016-11-04 14:38:31 +0200

    Clear cookie

--- web/Cookie.php
+++ web/Cookie.php
@@ -127,7 +127,7 @@ class Cookie extends LogAwareObject implements CacheInterface {
      */
     public function delete($key){
         $domain = ltrim($_SERVER['SERVER_NAME'], "www.");
-        setcookie($key, "", time(), '/', $domain, $this->secured, $this->httpOnly);
+        setcookie($key, "", time()-5, '/', $domain, $this->secured, $this->httpOnly);
     }
 
     /**
